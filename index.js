cfee8a0b0b9f17b0db14057dcc64b88765c33a9a
rajiv chaudhary <vijarchy@gmail.com>
2025-05-04 14:08:06 +0545

    just for js first day

---
+++ index.js
@@ -0,0 +1,9 @@
+/*there are three types of methods ro use js same as CSS*/
+console.log("hello babu");
+function bodycolor()
+{
+   let color = document.body.button.style.backgroundColor = "red" ;
+   console.log(color) ;
+}
+
+bodycolor();
