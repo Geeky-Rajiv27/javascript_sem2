2081d4c14a3a47a61aa8a1a45843c684d8a62159
rajiv chaudhary <vijarchy@gmail.com>
2025-05-18 21:26:14 +0545

    updated day10 assignments

--- day10/index10.js
+++ day10/index10.js
@@ -80,8 +80,8 @@ for(let i = 1; i <= 5; i++) {
 */
 
 //  -------------- -------------- -------------- -------------- --------------
-
-
+// to print the number except the multiple of 5
+/*
 for(let i = 1 ; i <= 15 ; i++)
 {
    if(i % 5 == 0)
@@ -91,3 +91,108 @@ for(let i = 1 ; i <= 15 ; i++)
     console.log(i);
    }
 }
+
+*/
+
+//assignment of day 10 
+
+/*
+Assignment 1 : Factorial Finder
+problem :
+
+Ask the user to enter a positive integer using prompt().Use a for loop
+to claculate the factorial of the number and display the result using 
+alert(). 
+*/
+/*
+
+let num = parseInt(prompt("Enter any number : "));
+let fact = 1;
+    if(num > 0)
+    { 
+        for(let i = 1; i <= num ; i++)
+           {
+         fact = fact * i  ;
+           }
+           console.log(fact);
+    }else{
+        alert("Invalid Input. ")
+    }
+*/
+
+// Assignment 2 : SUm of Odd numbers
+//problem:
+
+/*
+Ask the user to enter how many odd numbers to sum using prompt(). 
+Then calculate and display the sum of the first n odd numbers using a loop.
+*/
+
+/*
+let num = Number(prompt("Enter any number : "));
+let sum = 0 , count = 0;
+for(let i = 1 ; ; i = i + 2)  // here i have done increment by 2 that is only odd no. 
+     //        number will come so no need to check down using condition.
+{
+        sum = sum + i ;
+        count++ ;
+
+    if(count === num)   //  when the count that is unit becomes equal 
+//                    to the num given by the user then it stops .
+    {
+        break;
+    }
+}
+alert("the sum of "+(num)+" terms of odd number is : "+(sum) );
+
+*/
+
+
+
+// Assignment 3 : SUm of even numbers
+//problem:
+
+/*
+Ask the user to enter how many even numbers to sum using prompt(). 
+Then calculate and display the sum of the first n even numbers using a loop.
+*/
+
+/*
+
+let num = Number(prompt("Enter any number : "));
+let sum = 0 , count = 0;
+for(let i = 2 ; ; i = i + 2)  // here i have done increment by 2 that is only odd no. 
+     //        number will come so no need to check down using condition.
+{
+        sum = sum + i ;
+        count++ ;
+
+    if(count === num)   //  when the count that is unit becomes equal 
+//                    to the num given by the user then it stops .
+    {
+        break;
+    }
+}
+alert("the sum of "+(num)+" terms of even number is : "+(sum) );
+
+*/
+
+// Assignment 4 : Perfect Square Sum
+// Problem :
+  /*
+  Ask the user to enter a number n using prmpt().Then calculate the sum of the first n 
+  perfect squares (1^2 + 2^2 + 3^2 + ......+ n^2), and display it using alert().
+  */
+
+
+
+let num = Number(prompt("Enter any number : "));
+let sum = 0 , count = 0;
+for(let i = 1 ; i <= num; i++)
+{
+    sum = sum + (i*i) ;
+}
+alert("the sum of "+(num)+" terms of even number is : "+(sum) );
+
+
+
