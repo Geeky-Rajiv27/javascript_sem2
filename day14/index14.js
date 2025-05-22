c2c65c6f02b15e77ed05c72150d01eb403787a5d
rajiv chaudhary <vijarchy@gmail.com>
2025-05-22 14:11:54 +0545

    test

---
+++ day14/index14.js
@@ -0,0 +1,36 @@
+/*Example of function to add two numbers */
+/*
+
+function addNum(a, b)
+{
+    return (a+b);
+}
+
+let a = 5 ;
+let b = 5;
+let result = addNum(a,b);
+console.log("The sum of a and b is :"+(result));
+*/
+
+/*Example to call and print the name given by user */
+/*
+function display_Name(name)
+{
+    console.log("Your name is "+name);
+}
+let name = prompt("Enter your name : ");
+display_Name(name);
+*/
+
+// Calculate area of rectangle using function with arguments.
+/*
+function Area(l,b)
+{
+    return (l * b);
+}
+let l = prompt("Enter the length of Rectangle :");
+let b = prompt("Enter the breadth of Rectangle :");
+let result = Area(l,b);
+console.log("The area of the rectangle is "+(result));
+*/
+
